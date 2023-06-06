d2e7b97b3cafffd51c838c3fef9be259649dc032
maxamed1st <maxamed1st@icloud.com>
2023-06-06 20:51:31 +0100

    design header

---
+++ src/components/header.svelte
@@ -0,0 +1,10 @@
+<script>
+  import Stack from "./stack.svelte";
+</script>
+<header class="flex justify-center bg-[var(--secondary)] text-[var(--primary-dark)] p-5 sm:h-96 lg:h-[500px]">
+  <Stack />
+  <section class="flex flex-1 flex-col text-center">
+    <div class="my-auto "> 💆🏾‍♂️ </div>
+    <header class="font-bold "> Frontend Developer </header>
+  </section>
+</header>
