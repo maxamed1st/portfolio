9441cd9f280d63ed24df0eb7b75b73a968f16978
maxamed1st <maxamed1st@icloud.com>
2023-06-07 22:08:20 +0100

    create a card utility component

---
+++ src/lib/Card.svelte
@@ -0,0 +1,14 @@
+<article class="w-96 bg-white p-5 rounded-lg">
+    <slot name="image"></slot>
+    <header class="font-thin">
+        <slot name="title"></slot>
+    </header>
+    <main class="font-light break-all pb-2">
+        <slot name="description"> No description </slot>
+    </main>
+    <hr class="text-[var(--secondary)]" />
+    <footer class="flex [&_*]:flex-1 text-center ">
+            <slot name="demo"> </slot>
+            <slot name="github"> </slot>
+    </footer>
+</article>
