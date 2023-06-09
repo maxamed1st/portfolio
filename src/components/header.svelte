6bd65f7f66c94d530780893e15b9e55c871d2eda
maxamed1st <maxamed1st@icloud.com>
2023-06-09 17:28:10 +0100

    bigger text on wider screens

--- src/components/header.svelte
+++ src/components/header.svelte
@@ -3,7 +3,7 @@
 </script>
 <header class="flex justify-items-center bg-[var(--secondary)] text-[var(--primary-dark)] p-5 sm:h-96 lg:h-[500px]">
   <Stack />
-  <section class=" flex-1 text-center self-center">
+  <section class=" flex-1 self-center text-center sm:text-6xl">
     <div class="my-auto "> Mohamed Hassan Ahmed </div> 
     <div class="font-thin "> Frontend Developer </div>
   </section>
