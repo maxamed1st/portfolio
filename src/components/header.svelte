0b8d8b624104b59397c8d241c98a27a9b0c6d17b
maxamed1st <maxamed1st@icloud.com>
2023-06-09 17:24:00 +0100

    restyle header

--- src/components/header.svelte
+++ src/components/header.svelte
@@ -1,10 +1,10 @@
 <script>
-  import Stack from "./stack.svelte";
+  import Stack from "./Stack.svelte";
 </script>
-<header class="flex justify-center bg-[var(--secondary)] text-[var(--primary-dark)] p-5 sm:h-96 lg:h-[500px]">
+<header class="flex justify-items-center bg-[var(--secondary)] text-[var(--primary-dark)] p-5 sm:h-96 lg:h-[500px]">
   <Stack />
-  <section class="flex flex-1 flex-col text-center">
-    <div class="my-auto "> 💆🏾‍♂️ </div>
-    <header class="font-bold "> Frontend Developer </header>
+  <section class=" flex-1 text-center self-center">
+    <div class="my-auto "> Mohamed Hassan Ahmed </div> 
+    <div class="font-thin "> Frontend Developer </div>
   </section>
 </header>
