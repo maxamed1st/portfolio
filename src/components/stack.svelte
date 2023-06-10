cf5e59a0f5d9ad9c61559dde70e55b158e2e23d4
maxamed1st <maxamed1st@icloud.com>
2023-06-10 10:18:49 +0100

    account for safari users

--- src/components/stack.svelte
+++ src/components/stack.svelte
@@ -1,7 +1,7 @@
 <script>
     import Icons from "../assets/Icons.svelte";
 </script>
-<section id="STACK" class="grid grid-cols-4 grid-rows-3 aspect-square scale-50 gap-4 flex-1">
+<section id="STACK" class="grid grid-cols-4 grid-rows-3 aspect-square w-full h-full scale-50 gap-4 flex-1">
         <Icons i="html" fill="#E34F26" />
         <Icons i="css" fill="#1572B6" />
         <Icons i="tailwind" fill="#dd6f41" />
